853d0f2f2a47e31250996f30328d5330bed8ebbb
Xillyk <xillyk@hotmail.com>
2023-04-10 02:00:10 +0700

    tmp server

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const { logger } = require("./middleware/logEvents");
+const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
 
 // custom middleware logger
 app.use(logger);
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-// serve static file
-app.use(express.static(path.join(__dirname, "/public")));
+// cross origin resource sharing
+app.use(cors(corsOptions));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-  //res.sendFile('./views/index.html', { root: __dirname });
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+// built-in middleware to handler urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in for json
+app.use(express.json());
+
+// serve static file
+app.use("/", express.static(path.join(__dirname, "/public")));
+// app.use("/subdir", express.static(path.join(__dirname, "/public")));
+
+// routes
+app.use("/", require("./routes/root"));
+// app.use("/subdir", require("./routes/subdir"));
+app.use("/employees", require("./routes/api/employees"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 not found" });
+  } else if (req.accepts("txt")) {
+    res.send("404 not found");
+  }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
