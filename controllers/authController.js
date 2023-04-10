8a5a2798d86272e4c66774a7285c86bc0d327ea1
Xillyk <xillyk@hotmail.com>
2023-04-10 21:48:54 +0700

    implement jwt auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,10 @@ const usersDB = {
 };
 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const fsPromises = require("fs").promises;
+const path = require("path");
 
 const handleLogin = async (req, res) => {
   const { user, pwd } = req.body;
@@ -20,8 +24,47 @@ const handleLogin = async (req, res) => {
   // evaluate password
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
-    // create JWT
-    res.json({ success: `User ${user} is logged in.` });
+    // create JWTs
+    const accessToken = jwt.sign(
+      {
+        username: foundUser.username,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "30s",
+      }
+    );
+
+    const refreshToken = jwt.sign(
+      {
+        username: foundUser.username,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    const otherUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+
+    // saving refresh token with current user
+    usersDB.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
+
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,  // 1 day in ms unit
+    });
+    
+    res.json({
+      accessToken,
+    });
   } else {
     res.sendStatus(401);
   }
