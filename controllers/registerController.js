9b035d0d895be205c63db6a3f246a3687a116026
Xillyk <xillyk@hotmail.com>
2023-04-16 23:26:39 +0700

    implement check role

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -30,6 +30,7 @@ const handleNewUser = async (req, res) => {
     const newUser = {
       username: user,
       password: hashedPwd,
+      roles: { "User": 2001 },
     };
     usersDB.setUsers([...usersDB.users, newUser]);
 
