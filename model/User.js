d359c01af5f8406268e818742ebc25034fd7c525
Xillyk <xillyk@hotmail.com>
2023-04-17 05:30:27 +0700

    create User model

---
+++ model/User.js
@@ -0,0 +1,24 @@
+const moongose = require("mongoose");
+const Schema = moongose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  roles: {
+    User: {
+      type: Number,
+      default: 2001,
+    },
+    Editor: Number,
+    Admin: Number,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  refreshToken: String,
+});
+
+module.exports = moongose.model("User", userSchema);
